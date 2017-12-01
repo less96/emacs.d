da03a16dde6abdf3ab49dd64e43aa637bb624d68
mastize <mastizenone@gmail.com>
2017-11-30 20:50:46 -0800

    emacs setting update

---
+++ custom-set-variables.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(git-gutter:handled-backends (quote (svn hg git)))
+ '(package-selected-packages
+   (quote
+    (bash-completion org-jira ace-pinyin iedit package-lint emms dumb-jump web-mode pyim ivy-hydra hydra quack neotree flx-ido simple-httpd legalese elpy company-c-headers company cliphist git-link tagedit ace-mc multiple-cursors rjsx-mode js2-mode js-doc multi-term bookmark+ rvm dropdown-list flymake-lua pomodoro bbdb flyspell-lazy cpputils-cmake cmake-mode ace-window buffer-move counsel-gtags idomenu w3m unfill tidy session emmet-mode ruby-compilation groovy-mode regex-tool page-break-lines mwe-log-commands move-text less-css-mode ibuffer-vc hl-sexp counsel-bbdb find-file-in-project counsel swiper ivy flymake-ruby flymake-jslint flymake-css exec-path-from-shell git-timemachine dsvn textile-mode rainbow-delimiters scratch diminish htmlize dictionary connection link dired+ markdown-mode scss-mode haml-mode writeroom-mode nvm jump find-by-pinyin-dired pinyinlib findr erlang paredit yaml-mode workgroups2 robe lua-mode wgrep smex request haskell-mode gitignore-mode gitconfig-mode gist fringe-helper expand-region dash color-theme auto-yasnippet auto-compile async ace-link)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-delimiter-face ((t (:foreground "dark sea green" :slant italic))))
+ '(font-lock-comment-face ((t (:foreground "dark sea green" :slant italic))))
+ '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
